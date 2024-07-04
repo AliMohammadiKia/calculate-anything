a190fd8bcfeba8c81b20fba2e115aa4a12adef60
Niloufar-Heidarzade <Niiloufar.Heidarzadeh@gmail.com>
2024-07-04 16:37:05 +0330

    finish header and started the input box

--- Mortgage repayment/createForm.js
+++ Mortgage repayment/createForm.js
@@ -8,7 +8,7 @@ main.classList = "w-full flex justify-center items-center"
 body.append(main);
 //primary container
 const container = document.createElement("section");
-container.className = "bg-white  grid md:grid-cols-2 shadow-md rounded-xl  md:w-[700px] sm:w-4/5 w-full";
+container.className = "container bg-white  grid md:grid-cols-2 shadow-md rounded-xl sm:w-11/12 md:w-[700px] w-full";
 main.append(container);
 //inside container we create 2 divs
 const newDivOne = document.createElement("div");
@@ -23,14 +23,40 @@ form.classsList = "flex"
 newDivOne.append(form);
 //create the header of the form
 const formHeader = document.createElement("div");
+formHeader.classList = "flex justify-between"
 form.append(formHeader);
+//form title
 const title = document.createElement("h4");
 title.innerText = "Mortgage Calculator";
 title.classList = "text-slate-900 font-bold"
 formHeader.append(title);
+//clear all button
 const clearButton = document.createElement("button");
 clearButton.innerText = "Clear All";
-clearButton.classList = ""
+clearButton.classList = "text-xs underline text-slate-600"
 formHeader.append(clearButton);
+clearButton.onsubmit = "false";
+clearButton.addEventListener("click",(e)=>{
+  e.preventDefault();
+  //empty inputs and reset the mortgage type
+});
 //set the font family
 body.style.fontFamily = "Plus Jakarta Sans";
+//mortgage amount input box
+const mortgageAmount = document.createElement("div");
+mortgageAmount.classList = "flex flex-col"
+const label1 = document.createElement("label");
+label1.innerText = "Mortgage Amount";
+label1.for = "amount";
+label1.classList = "text-xs text-slate-600 mt-4";
+const inputBox = document.createElement("div");
+inputBox.classList = "relative";
+const beforeInput1 = document.createElement("div");
+beforeInput1.classList = "absolute left-0 top-0 bg-[#e3f3fd] w-[40px] h-[26px] flex items-center justify-center text-slate-800 rounded-l border border-slate-400"
+const amountInput = document.createElement("input");
+amountInput.id = "amount";
+amountInput.classList =  "pl-[40px] border-slate-400 border rounded";
+inputBox.append(beforeInput1 , amountInput);
+mortgageAmount.append(label1 , inputBox);
+form.append(mortgageAmount);
+
